824d2a96ef106122d81057e83d105ad36137adea
TongtongSong <songtongmail@163.com>
2021-10-14 15:05:19 +0800

    modify slience

--- src/audio/remove_audio_slience/remove_audio_slience.sh
+++ src/audio/remove_audio_slience/remove_audio_slience.sh
@@ -10,27 +10,29 @@
 # or . remove_audio_slience.sh \
 #    wav_dir textgrid_for_cv
 #########################################
-
-input=$1
-txt_grid_dir=$2
+dir=$1
+name=$2
+txt_grid_dir=$3
 mkdir -p $txt_grid_dir
-nj=32
+nj=16
 tmpdir=$(mktemp -d tmp-XXXXX)
-[ -d $input ] && find $input -iname "*.wav" > $tmpdir/wav_list && input=$tmpdir/wav_list
+[ -d $dir/$name ] && find $dir/$name -iname "*.wav" > $tmpdir/wav_list && input=$tmpdir/wav_list
 
 # your JOB
 _remove_audio_slience(){
   local wav_path=$1
-  for wav in `cat $wav_path`;do
+  local dir_name=$2
+  for wav in $(cat $wav_path);do
   {
-      name=`echo $wav|awk -F '/' '{print $NF}'|cut -d'.' -f1`
+      name=$(echo $wav|awk -F '/' '{print $NF}')
+      echo $wav
       ./praat_nogui --run annotate_silences.praat $wav $txt_grid_dir/${name}.TextGrid 100 0.0 -25.0 0.1 0.1 silence sound
-      time=`python3 readTextGrid.py $txt_grid_dir/${name}.TextGrid`
-      start=`echo $time|cut -d' ' -f1`
-      end=`echo $time|cut -d' ' -f2`
+      time=$(python3 readTextGrid.py $txt_grid_dir/${name}.TextGrid)
+      start=$(echo $time|cut -d' ' -f1)
+      end=$(echo $time|cut -d' ' -f2)
       echo $name $start $end
-      new_wav=`echo $wav|sed 's|librispeech|librispeech-no-slience|'` ## modify yourself
-      new_dir=`dirname $new_wav`
+      new_wav=$(echo $wav|sed 's|'$dir_name'|'${dir_name}-no-slience'|') ## modify yourself
+      new_dir=$(dirname $new_wav)
       [ ! -d $new_dir ] && mkdir -p $new_dir
       sox $wav $new_wav trim $start =$end
   };done
@@ -38,9 +40,9 @@ _remove_audio_slience(){
 
 # multithread
 split --additional-suffix .slice -d -n l/$nj $input $tmpdir/tmp_
-for slice in `ls $tmpdir/tmp_*.slice`; do
+for slice in $(ls $tmpdir/tmp_*.slice); do
 {
-  _remove_audio_slience $slice
+  _remove_audio_slience $slice $name
 }&
 done
 wait
